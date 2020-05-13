be5ff1fdd9083f38e87cf689c14472cb4a1c8790
FUMI-POPPIN <fumitake.kawasaki@el-soul.com>
2020-05-13 18:27:13 +0900

    add todays transaction scraper

--- lib/rakuten_securities_scraper.rb
+++ lib/rakuten_securities_scraper.rb
@@ -11,6 +11,66 @@ module RakutenSecuritiesScraper
       }
     end
 
+    def todays_transaction
+      driver = get_selenium_driver(:chrome)
+      wait = Selenium::WebDriver::Wait.new(timeout: 100)
+      url = "https://www.rakuten-sec.co.jp/ITS/V_ACT_Login.html"
+      driver.get url
+      driver.find_element(id: "form-login-id").send_keys @login[:login_id]
+      driver.find_element(id: "form-login-pass").send_keys @login[:login_pw]
+      driver.find_element(id: "login-btn").click
+      sleep 2
+      driver.action.move_to(driver.find_element(id: "gmenu_domestic_stock")).perform
+      sleep 2
+      driver.find_element(id: "gmenu_domestic_stock").click
+      sleep 2
+      driver.find_element(id: "nav-sub-menu-order-arrow").click
+      wait.until { driver.find_element(xpath: "//*[@id='str-main-inner']/table/tbody/tr/td/form/div[2]/div/ul/li[3]/a") }
+      driver.find_element(xpath: "//*[@id='str-main-inner']/table/tbody/tr/td/form/div[2]/div/ul/li[3]/a").click
+      sleep 2
+      span_select = Selenium::WebDriver::Support::Select.new(driver.find_element(:id, "orderStatus"))
+      span_select.select_by(:value, "5")
+      sleep 1
+      driver.find_element(css: "input[type='image']").click
+      sleep 2
+      tables = driver.find_elements(xpath: "//tr")
+      page_num = driver.find_element(class_name: "mtext").text.scan(/(.+)件中/)[0][0].to_i
+      row_nums = (21..(21 + (page_num * 3) -1)).to_a
+      records = []
+      row_nums.each_slice(3).to_a.map do |col_num|
+        detail = {}
+        tables[col_num[0]..col_num[2]].map.with_index do |row, i|
+          cells = row.find_elements(:css, "td").map(&:text)
+          case i
+          when 0
+            detail[:order_id] = cells[0] if cells[0]
+            detail[:order_status] = cells[1] if cells[1]
+            detail[:order_date] = cells[2] if cells[2]
+            detail[:stock_name] = cells[3].split("\n")[0] if cells[3]
+            detail[:code] = cells[3].split("\n")[1].split(" ")[0] if cells[3]
+            detail[:market] = cells[3].split("\n")[1].split(" ")[1] if cells[3]
+            detail[:trade_type] = cells[4] if cells[4]
+            detail[:buy_sell] = cells[5] if cells[5]
+            detail[:order_amount] = cells[7] if cells[7]
+            detail[:unit_price] = cells[8] if cells[8]
+            detail[:executed_price] = cells[8] if cells[8]
+          when 1
+            detail[:order_condition] = cells[2] if cells[2]
+            detail[:account_type] = cells[3] if cells[3]
+            detail[:executed_price] = cells[5] if cells[5]
+            detail[:fee] = cells[6] if cells[6]
+          else
+            detail[:order_expire_date] = cells[2] if cells[2]
+            detail[:order_type] = cells[3] if cells[3]
+          end
+          records << detail
+        end
+      end
+      records
+    ensure
+      driver.quit
+    end
+
     def all_history
       driver = get_selenium_driver(:chrome)
       wait = Selenium::WebDriver::Wait.new(timeout: 100)
@@ -47,9 +107,11 @@ module RakutenSecuritiesScraper
       driver.find_element(id: "login-btn").click
       wait.until { driver.find_element(id: "header-menu-button-asset") }
       driver.action.move_to(driver.find_element(id: "header-menu-button-asset")).perform
+      sleep 2
       wait.until { driver.find_element(xpath: "/html/body/div[2]/div/div/div[2]/ul[1]/li[1]/ul/li[1]/a") }
+      sleep 2
       driver.find_element(xpath: "/html/body/div[2]/div/div/div[2]/ul[1]/li[1]/ul/li[1]/a").click
-
+      sleep 3
       driver.find_element(css: "input[type='image']").click
       sleep 2
       rows = driver.find_elements(xpath: "//tr")
@@ -89,12 +151,17 @@ module RakutenSecuritiesScraper
             tax_type: cells[7].split("\n")[1],
             total_price: cells[8]
           }
-          return trade_records if page_num < 20
         end
+        puts trade_records
+        return trade_records if page_num < 20
         driver.find_element(link: "次の20件").click
       end
       sleep 2
-      driver.find_element(link: "次の#{run_times[1]}件").click
+      begin
+        driver.find_element(link: "次の#{run_times[1]}件").click
+      rescue
+        return trade_records
+      end
       rows = driver.find_elements(xpath: "//tr")
       rows[10.."1#{run_times[1]}".to_i-1].map do |row|
         cells = row.find_elements(:css, "td").map { |a| a.text.strip.gsub(",", "") }
--- spec/rakuten_securities_scraper_spec.rb
+++ spec/rakuten_securities_scraper_spec.rb
@@ -2,17 +2,22 @@ RSpec.describe RakutenSecuritiesScraper do
   before(:each) do
     @a1 = RakutenSecuritiesScraper::RakutenScraper.new(ENV["LOGIN_ID"], ENV["LOGIN_PW"])
   end
-  it "has a version number" do
-    expect(RakutenSecuritiesScraper::VERSION).not_to be nil
-  end
+  # it "has a version number" do
+  #   expect(RakutenSecuritiesScraper::VERSION).not_to be nil
+  # end
 
-  it "Get All Trade History" do
-    a1 = @a1.all_history
-    expect(a1[0][:stocks].to_i).to be > 0
-  end
+  # it "Get All Trade History" do
+  #   a1 = @a1.all_history
+  #   expect(a1[0][:stocks].to_i).to be > 0
+  # end
 
-  it "Get Today's Trade History" do
-    a1 = @a1.todays_history
-    expect(a1).to eq "no data"
+  # it "Get Today's Trade History" do
+  #   a1 = @a1.todays_history
+  #   expect(a1).to eq "no data"
+  # end
+
+  it "Get Today's Transaction" do
+    a1 = @a1.todays_transaction
+    expect(a1[0][:order_id].size).to eq 4
   end
 end
